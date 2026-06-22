eb265bcc30039ffc770226594e69eaa6a421c78e
arzhanik <arzhanik.nalbandyan@gmail.com>
2026-06-23 02:33:54 +0400

    Added two methods in RuleEngine file.

--- MiniRuleEngine/RuleEngine.cs
+++ MiniRuleEngine/RuleEngine.cs
@@ -23,4 +23,63 @@ public class RuleEngine
 
         _rules[_count++] = rule;
     }
+
+    public void ValidateFailFast(IEntity entity)
+    {
+        for (int i = 0; i < _count; ++i)
+        {
+            if (!_rules[i].AppliesTo(entity))
+            {
+                continue;
+            }
+
+            try
+            {
+                _rules[i].Check(entity);
+            }
+            catch (RuleViolationException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new RuleViolationException(_rules[i].Name, $"Unexpected rule violation: {ex.Message}");
+            }
+        }
+    }
+    public void ValidateCollectAll(IEntity entity)
+    {
+        int ViolationsCount = 0;
+        RuleViolationException[] violations = new RuleViolationException[_count];
+        for (int i = 0; i < _count; ++i)
+        {
+            if (!_rules[i].AppliesTo(entity))
+            {
+                continue;
+            }
+
+            try
+            {
+                _rules[i].Check(entity);
+            }
+            catch (RuleViolationException violation)
+            {
+                violations[ViolationsCount++] = violation;
+            }
+            catch (Exception ex)
+            {
+                violations[ViolationsCount++] = new RuleViolationException(_rules[i].Name, $"Unexpected rule error: {ex.Message}");
+            }
+        }
+
+        if (ViolationsCount > 0)
+        {
+            RuleViolationException[] exact = new RuleViolationException[ViolationsCount];
+            for (int i = 0; i < ViolationsCount; i++)
+            {
+                exact[i] = violations[i];
+            }
+            throw new EntityValidationException(entity, exact);
+        }
+    }
 }
